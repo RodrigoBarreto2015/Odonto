626bc93ffef3273b5078d196d41a7a3581a72735
RodrigoBarreto2015 <rodrigobarreto2012@hotmail.com>
2024-04-21 11:12:39 -0300

    feat: colapse menu when cliked item

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -6,8 +6,15 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import BrandIcon from "../../assets/images/brand.png";
 import "./NavBar.css"
 import { Link, Outlet } from "react-router-dom";
+import { useRef } from "react";
 
 const NavbarOs = () => {
+
+    const collapseRef = useRef(null);
+
+  const hideBars = () => {
+    collapseRef.current.setAttribute("class", "navbar-collapse collapse");
+  };
 
     return (
         <>
@@ -22,23 +29,23 @@ const NavbarOs = () => {
                             className="d-inline-block align-top"
                         />
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" style={{border: 'none'}}/>
-                    <Navbar.Collapse id="basic-navbar-nav" >
-                        <Nav className="ms-auto" style={{textAlign: 'center' }}>
-                        <Nav.Link as={Link} className="btn-agendar" to="https://api.whatsapp.com/send?phone=5585987550023&text=Ol%C3%A1%2C+gostaria+de+agendar+uma+consulta%3F"><span>Agendar consulta</span></Nav.Link>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" style={{ border: 'none' }} />
+                    <Navbar.Collapse id="basic-navbar-nav" ref={collapseRef}>
+                        <Nav className="ms-auto" style={{ textAlign: 'center' }}>
+                            <Nav.Link as={Link} className="btn-agendar" to="https://api.whatsapp.com/send?phone=5585987550023&text=Ol%C3%A1%2C+gostaria+de+agendar+uma+consulta%3F"><span>Agendar consulta</span></Nav.Link>
                             <NavDropdown title="Consultas e tratamentos" id="basic-nav-dropdown">
-                                <NavDropdown.Item as={Link} to="/servicos/implante">Implante dentário</NavDropdown.Item>
-                                <NavDropdown.Item as={Link} to="/servicos/aparelho">Aparelho ortodôntico</NavDropdown.Item>
-                                <NavDropdown.Item as={Link} to="/servicos/clareamento">Clareamento dentário</NavDropdown.Item>
-                                <NavDropdown.Item as={Link} to="/servicos/cirurgias">Cirurgias e extrações</NavDropdown.Item>
-                                <NavDropdown.Item as={Link} to="/servicos/profilaxia">Profilaxia (Limpeza)</NavDropdown.Item>
-                                <NavDropdown.Item as={Link} to="/servicos/protese">Prótese dentária</NavDropdown.Item>
-                                <NavDropdown.Item as={Link} to="/servicos/restauracao">Restauração dental</NavDropdown.Item>
-                                <NavDropdown.Item as={Link} to="/servicos/tratamento">Tratamento de canal</NavDropdown.Item>
-                                <NavDropdown.Item as={Link} to="/servicos/lentes">Lentes de contato dental</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} onClick={hideBars} to="/servicos/implante">Implante dentário</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} onClick={hideBars} to="/servicos/aparelho">Aparelho ortodôntico</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} onClick={hideBars} to="/servicos/clareamento">Clareamento dentário</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} onClick={hideBars} to="/servicos/cirurgias">Cirurgias e extrações</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} onClick={hideBars} to="/servicos/profilaxia">Profilaxia (Limpeza)</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} onClick={hideBars} to="/servicos/protese">Prótese dentária</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} onClick={hideBars} to="/servicos/restauracao">Restauração dental</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} onClick={hideBars} to="/servicos/tratamento">Tratamento de canal</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} onClick={hideBars} to="/servicos/lentes">Lentes de contato dental</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link as={Link} to="/sobrenos">Sobre nós</Nav.Link>
-                            <Nav.Link as={Link} to="/contato">Contato</Nav.Link>
+                            <Nav.Link as={Link} onClick={hideBars} to="/sobrenos">Sobre nós</Nav.Link>
+                            <Nav.Link as={Link} onClick={hideBars} to="/contato">Contato</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
