3a0643146cb463674e0142bae63144e79e3e222e
RodrigoBarreto2015 <rodrigobarreto2012@hotmail.com>
2024-04-27 08:29:53 -0300

    fix: subheader text change

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,7 +15,7 @@ export const Header = () => {
       </header>
       <div className="vstack gap-3">
         <div className="p-2">Nossa equipe está disponível para atende-lo de segunda a sexta-feira,
-          das 08hrs às 12hrs e das 14hrs às 18hrs.</div>
+          das 08hrs às 12hrs e das 14hrs às 18hrs. E aos sábados de 08hrs às 12hrs.</div>
         <div className="p-2">Agende sua consulta conosco hoje mesmo e
           descubra como podemos ajuda-lo a manter um sorriso saudável e bonito!</div>
       </div>
