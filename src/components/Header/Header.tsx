ab630f13c5a2cbedfeaa0f3bbc93b057151340e0
RodrigoBarreto2015 <rodrigobarreto2012@hotmail.com>
2024-05-03 09:46:54 -0300

    style: header image style change

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,15 +5,15 @@ export const Header = () => {
     <>
       <header>
         <div className="text-header">
-          <p>Nossa equipe está disponível para atende-lo de segunda a sexta-feira,
-            das 08hrs às 12hrs e das 14hrs às 18hrs.</p>
-          <span>
+          <span className="title">Nossa equipe está disponível para atende-lo de segunda a sexta-feira,
+            das 08hrs às 12hrs e das 14hrs às 18hrs.</span><br /><br />
+          <span className="subtitle">
             Agende sua consulta conosco hoje mesmo e
             descubra como podemos ajuda-lo a manter um sorriso saudável e bonito!
           </span>
         </div>
       </header>
-      <div className="vstack gap-3">
+      <div className="vstack gap-1">
         <div className="p-2">Nossa equipe está disponível para atende-lo de segunda a sexta-feira,
           das 08hrs às 12hrs e das 14hrs às 18hrs. E aos sábados de 08hrs às 12hrs.</div>
         <div className="p-2">Agende sua consulta conosco hoje mesmo e
